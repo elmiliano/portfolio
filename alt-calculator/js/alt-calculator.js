00be0cc6b7eaaef6183e14b76f759d58938743e9
Emiliano Machado <emiliano.a.machado@gmail.com>
2024-01-09 16:24:38 -0800

    [ADD - HTML and CSS dir and files]

--- alt-calculator/js/alt-calculator.js
+++ alt-calculator/js/alt-calculator.js
@@ -14,7 +14,18 @@ function calculate(expression) {
   }
 }
 
-function getOperands(expression, index) {                 // takes an expression and an index, returns the next two operands
+function handleOperator(expression, index) {                 // takes an expression and an index, returns the next two operands
+
+  flag = false;
+
+  for (i = 0; i < 2; i++) {
+    flag = getOperands(expression, index);
+
+    if (flag) {
+      index += 2;
+    }
+  }
+
   if (operators.includes(expression[index+1])) {           
     operands.push(checkOperator(expression, index+1));    // recursive function (checkOperator) pushes operands
     index += 2;                                           // skips the operator's next two operands
@@ -31,8 +42,18 @@ function getOperands(expression, index) {                 // takes an expression
   return [Number.parseInt(operands.pop()), Number.parseInt(operands.pop())]; // returns numbers as a list of ints
 }
 
+function getOperands(expression, index) {
+  if (operators.includes(expression[index+1])) {           
+    operands.push(checkOperator(expression, index+1));    // recursive function (checkOperator) pushes operands
+    return true;                                          // skips the operator's next two operands
+  } else {
+    operands.push(expression[index+1]);    
+    return false;                                         // pushes next character
+  }
+}
+
 function checkOperator(expression, index) {               // gets an expression's operands and returns the result
-  temp = getOperands(expression, index);                  // gets an expression's operands (a, b)
+  temp = handleOperator(expression, index);               // gets an expression's operands (a, b)
   
   switch (expression[index]) {                            // performs operation depending on operator (+, -, *, /)
     case '+':
